90ae0c0e5402c1448b4a8a8e9e8d440d95808bed
0xEbrahim <elsayedebrahim603@gmail.com>
2024-02-14 17:42:27 +0200

    refactor

--- src/controllers/Auth/changePassword.js
+++ src/controllers/Auth/changePassword.js
@@ -1,6 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 import asyncHandler from "express-async-handler";
-import APIError from "../../utils/APIError.js";
 import { hashPassword } from "../../utils/hashingPassword.js";
 const prisma = new PrismaClient();
 
