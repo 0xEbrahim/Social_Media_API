45729bcf5fddd6b3800bd86d5928cd80126d589a
0xEbrahim <elsayedebrahim603@gmail.com>
2024-03-05 02:43:25 +0200

    start: Working on comment & like services

--- src/hooks/index.hooks.js
+++ src/hooks/index.hooks.js
@@ -4,12 +4,16 @@ import userRouter from "../routes/user.routes.js";
 import postRouter from "../routes/post.routes.js";
 import followRouter from "../routes/follow.routes.js";
 import storyRouter from "../routes/story.routes.js";
+import likeRouter from "../routes/like.routes.js";
+import commentRouter from "../routes/comment.routes.js";
 export default (app) => {
   app.use("/api/v1/auth", authRouter);
   app.use("/api/v1/user", userRouter);
   app.use("/api/v1/post", postRouter);
   app.use("/api/v1/follow", followRouter);
   app.use("/api/v1/story", storyRouter);
+  app.use("/api/v1/like", likeRouter);
+  app.use("/api/v1/comment", commentRouter);
   app.all("*", notFound);
   app.use(errorHandler);
 };
